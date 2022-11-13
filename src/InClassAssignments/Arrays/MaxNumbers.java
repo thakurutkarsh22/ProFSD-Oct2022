be88191ac513e65a1546a19a00911304e8879a80
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2022-11-13 23:16:41 +0530

    max numbers assignemnt another way

--- src/InClassAssignments/Arrays/MaxNumbers.java
+++ src/InClassAssignments/Arrays/MaxNumbers.java
@@ -27,20 +27,57 @@ public class MaxNumbers {
 
 
             //  algo to find out 3 max numbers....
-
-            Arrays.sort(arr);
-
-            int len = arr.length;
-
-            System.out.print(
-                    arr[len-1]
-                            + " " + arr[len-2]
-                            + " " + arr[len-3]
-            );
-            System.out.println();
+//            maxNumebrWay1(arr); good
+            maxNumebrWay2(arr); // not good
 
 
             T--;
         }
+    }
+
+    public static void maxNumebrWay1(int[] arr) {
+        Arrays.sort(arr);
+
+        int len = arr.length;
+
+        System.out.print(
+                arr[len-1]
+                        + " " + arr[len-2]
+                        + " " + arr[len-3]
+        );
+        System.out.println();
+    }
+
+//    another way to solve this ques
+//    find max, 2nd max, 3rdmax in an array.
+
+    public static void maxNumebrWay2(int[] arr) {
+
+        int[] ansArr = new int[3];
+
+        for(int repeat = 0; repeat <3 ;repeat++) {
+            int max = arr[0];
+            int maxIndex = 0;
+
+            for(int i = 0; i< arr.length; i++) {
+                if(arr[i] > max) {
+                    max = arr[i];
+                    maxIndex = i;
+                }
+            }
+
+//        we have to add the max in the storage
+            ansArr[repeat] = max;
+            arr[maxIndex] = Integer.MIN_VALUE; // -infinity
+        }
+
+//        ans array printing
+
+        for (int i = 0; i < ansArr.length; i++) {
+            System.out.print(ansArr[i] + " ");
+        }
+        System.out.println();
+
+
     }
 }

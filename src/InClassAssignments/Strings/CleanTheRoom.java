3b979b44bdc07f62622c8967a67da7bb2b543d8a
Vishal Soni <vs332837@gmail.com>
2022-11-28 08:41:24 +0530

    Update CleanTheRoom.java
    update clean the room with O(N) TC

--- src/InClassAssignments/Strings/CleanTheRoom.java
+++ src/InClassAssignments/Strings/CleanTheRoom.java
@@ -33,4 +33,22 @@ public class CleanTheRoom {
         }
         return max + 1;
     }
+    public static void cleanTheRoomV() throws Exception{
+        
+        // TC = O(N)
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(br.readLine());
+        
+        HashMap<String,Integer> hm = new HashMap<>();
+        
+        for(int i=0; i<size; i++){
+            String str = br.readLine();
+            hm.put(str,hm.getOrDefault(str,0)+1);
+        }              
+
+        // now find the max value
+        int result = Collections.max(hm.values());
+        System.out.println(result);
+   
+    }
 }

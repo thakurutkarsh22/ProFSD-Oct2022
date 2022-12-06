6a77bc337a197e12ce50ee4b1811ce037d21edc0
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2022-12-06 18:00:32 +0530

    contest questions (dec 4)

---
+++ src/Contest/dec4/CountingZerosToOnes.java
@@ -0,0 +1,47 @@
+package Contest.dec4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CountingZerosToOnes {
+        public static void main (String[]args){
+            // Your code here
+            Scanner scn = new Scanner(System.in);
+            int n = scn.nextInt();
+            int q = scn.nextInt();
+
+//        int ans = n * n; // 9
+
+            int[] doneRows = new int[n];
+            int[] doneCols = new int[n];
+
+
+            long blankRow = n;
+            long blankCol = n;
+
+            while (q != 0) {
+                int i = scn.nextInt(); // 1
+                int j = scn.nextInt(); // 1
+
+                int ithIndex = i - 1;
+                int jthIndex = j - 1;
+
+                if (doneRows[ithIndex] == 0) {
+                    blankRow--;
+                    doneRows[ithIndex] = 1;
+                }
+
+                if (doneCols[jthIndex] == 0) {
+                    blankCol--;
+                    doneCols[jthIndex] = 1;
+                }
+                long ans = blankRow * blankCol;
+                System.out.print(ans + " ");
+
+                q--;
+            }
+// TC => O(q)
+// SC =>  O(n)
+
+        }
+}
---
+++ src/Contest/dec4/MergeList.java
@@ -0,0 +1,57 @@
+package Contest.dec4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MergeList {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner scn = new Scanner(System.in);
+        int len1 = scn.nextInt();
+        int len2 = scn.nextInt();
+
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>();
+
+        for(int i=0;i<len1;i++) {
+            arr1.add(scn.nextInt());
+        }
+
+        for(int i=0;i<len2;i++) {
+            arr2.add(scn.nextInt());
+        }
+
+        int ind = scn.nextInt();
+
+        ArrayList<Integer> ans = mergeList(arr1,arr2,ind);
+
+        for(int i =0; i<ans.size() ;i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+    }
+
+    public static ArrayList<Integer> mergeList(ArrayList<Integer> arr1, ArrayList<Integer> arr2, int index) {
+        ArrayList<Integer> ansList = new ArrayList<>();
+
+        // add every element BEFORE index from arr1...
+
+        for(int i =0; i<index;i++) {
+            ansList.add(arr1.get(i));
+        }
+
+        // add the element form arr2 at index value ...
+
+        for(int i =0; i<arr2.size() ;i++) {
+            ansList.add(arr2.get(i));
+        }
+
+
+        // add every element AFTER index from arr1...
+        for(int i =index; i<arr1.size() ;i++) {
+            ansList.add(arr1.get(i));
+        }
+
+        return ansList;
+    }
+
+}
